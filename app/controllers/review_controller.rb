34c4db8cbf670a799d18d268eb7e8d2f430c9c27
Chris Fitzpatrick <chrisfitzpat@gmail.com>
2013-09-05 23:17:24 +0200

    fixing csv review

--- app/controllers/review_controller.rb
+++ app/controllers/review_controller.rb
@@ -1,3 +1,4 @@
+require 'csv'
 class ReviewController < ApplicationController
 
   
@@ -16,18 +17,15 @@ class ReviewController < ApplicationController
     
     anno = @notes.collect { |n| n.annotations }
     @annotations = anno.flatten
-  
-    csv = [["marc_record id", "note id", "record title", "record language",  "credit", "annotated role language", "annotated name", 'annotated canonical name',  "annotated role", "annotated role translation", "approved", "comment" ]]
-    @annotations.each do |annotation| 
-      marc_id = annotation.note.record.record_id 
-      csv << [marc_id, annotation.note.id , annotation.note.record_title, annotation.note.language, annotation.note.text, annotation.role_language, annotation.name, annotation.canonical_name, annotation.role, annotation.role_translation, annotation.approved?, annotation.comment ]
-    end  
     
     @csv = ""
-    csv.each { |cs|  @csv << cs.join(",") + "\n"  }
+    @csv << ["marc_record id", "note id", "record title", "record language",  "credit", "annotated role language", "annotated name", 'annotated canonical name',  "annotated role", "annotated role translation", "approved", "comment" ].to_csv
+    @annotations.each do |annotation| 
+      marc_id = annotation.note.record.record_id 
+      @csv << [marc_id, annotation.note.id , annotation.note.record_title, annotation.note.language, annotation.note.text, annotation.role_language, annotation.name,
+         annotation.canonical_name, annotation.role, annotation.role_translation, annotation.approved?, annotation.comment ].map { |c| c.to_s.encode("UTF-8") }.to_csv
+    end  
     
-    #render text: @csv, layout: false, content_type: "text/plain"
-  
      respond_to do |format|
        format.csv { render text: @csv }
        #format.html { render layout: false }
