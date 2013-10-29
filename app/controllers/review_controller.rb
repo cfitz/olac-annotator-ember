f328c91c54ac44e965d5db8b1985e530e595e10e
Chris Fitzpatrick <chrisfitzpat@gmail.com>
2013-10-29 12:25:50 +0100

    adding # of approved annotations to review

--- app/controllers/review_controller.rb
+++ app/controllers/review_controller.rb
@@ -19,11 +19,11 @@ class ReviewController < ApplicationController
     @annotations = anno.flatten
     
     @csv = ""
-    @csv << ["marc_record id", "note id", 'annotation id', 'creation date', "record title", "record language",  "credit", "annotated role language", "annotated name", 'annotated canonical name',  "annotated role", "annotated role translation", "approved", "comment" ].to_csv
+    @csv << ["marc_record id", "note id", 'annotation id', 'creation date', "record title", "record language",  "credit", "annotated role language", "annotated name", 'annotated canonical name',  "annotated role", "annotated role translation", "approved", "note's number of approved annotations", "comment" ].to_csv
     @annotations.each do |annotation| 
       marc_id = annotation.note.record.record_id 
       @csv << [marc_id, annotation.note.id , annotation.id, annotation.attributes['created_at'], annotation.note.record_title, annotation.note.language, annotation.note.text, annotation.role_language, annotation.name,
-         annotation.canonical_name, annotation.role, annotation.role_translation, annotation.approved?, annotation.comment ].map { |c| c.to_s.encode("UTF-8") }.to_csv
+         annotation.canonical_name, annotation.role, annotation.role_translation, annotation.approved?, annotation.note.approved_counter, annotation.comment ].map { |c| c.to_s.encode("UTF-8") }.to_csv
     end  
     
      respond_to do |format|
